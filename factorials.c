2357ec477c7be8206b8cd5204f846893c94e807b
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-02 23:30:24 +0500

    Update factorials.c

--- factorials.c
+++ factorials.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 // this code prints the factorail of numbers from zero till n numbers
 int main(){
-	int a[1000];
-	int n,i,f,j;
+	long long int a[1000], f;
+	int n,i,j;
 	printf("please enter till where do you want the factorials=");
 	scanf("%d",&n);
 	a[0]=1;
-	printf("%d\n",a[0]);
+	printf("%lld\n",a[0]);
 	i=1;
 	while(i<=n){
 		j=i;
@@ -16,7 +16,7 @@ int main(){
 			j--;
 		}
 		a[i]=f;
-		printf("%d\n", a[i]);
+		printf("%lld\n", a[i]);
 		i++;
 	}
 }
